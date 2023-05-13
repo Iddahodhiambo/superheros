718f9a2b2323192ae9e90d4190f2b2d14cb70f82
Iddahodhiambo <iddahawuor@yahoo.com>
2023-05-13 13:54:47 +0300

    get/ powers/id

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,7 +1,23 @@
 class PowersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     def index
 
         powers =Power.all
         render json: powers
     end
+
+    def show
+        power = find_power
+        render json: power
+    end
+
+       
+    private 
+  def find_power 
+    Power.find(params[:id])
+  end
+
+  def render_not_found
+    render json: { error: "Power not found" }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :heros,only: [:index, :show]
-  resources:powers,only: [:index]
+  resources:powers,only: [:index, :show]
 end
